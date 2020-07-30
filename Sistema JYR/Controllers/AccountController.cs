fff3d2c6caadd334ed465d3573ecb50a650af359
KEYLOR ALBERTO MARTINEZ RODRIGUEZ <komartinez@est.utn.ac.cr>
2020-07-30 16:02:18 -0600

    Ortografía, displayname, filtros

--- Sistema JYR/Controllers/AccountController.cs
+++ Sistema JYR/Controllers/AccountController.cs
@@ -228,7 +228,7 @@ namespace Sistema_JYR.Controllers
         {
             if (userId == null || code == null)
             {
-                string callbackUrl = await EnviarCorreoAsync(userId, "Confirma tú cuenta-Reenviado", 1);
+                string callbackUrl = await EnviarCorreoAsync(userId, "Confirma tu cuenta- reenviado", 1);
                 Session["MensajeIndex"] = "Error Confirmar Cuenta";
                 return RedirectToAction("Index", "Home");
             }
@@ -240,7 +240,7 @@ namespace Sistema_JYR.Controllers
             }
             else
             {
-                string callbackUrl = await EnviarCorreoAsync(userId, "Confirma tú cuenta-Reenviado", 1);
+                string callbackUrl = await EnviarCorreoAsync(userId, "Confirma tu cuenta- reenviado", 1);
                 Session["MensajeIndex"] = "Error Confirmar Cuenta";
                 return RedirectToAction("Index", "Home");
             }
